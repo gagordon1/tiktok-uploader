fea32c4595a29698a454997a006ad4a1878ba02c
Garrett Gordon <gagordon@mit.edu>
2022-09-11 18:24:12 -0400

    debugging upload page

--- src/main.js
+++ src/main.js
@@ -1,52 +1,61 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const puppeteer = __importStar(require("puppeteer"));
-const goToTikTok = async (page) => {
-    await page.goto('https://tiktok.com');
-    await page.pdf({ path: 'tiktok.pdf' });
+const puppeteer_extra_1 = __importDefault(require("puppeteer-extra"));
+const fs = require('fs').promises;
+const TIKTOKURL = "https://www.tiktok.com";
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const visitTikTok = async (page) => {
+    console.log("navigating to tiktok");
+    await page.goto(TIKTOKURL, {
+        waitUntil: 'networkidle0'
+    });
+    await page.pdf({ path: './screenshots/tiktok.pdf' });
 };
-const login = async (page, account, password) => {
+const clickUpload = async (page) => {
+    console.log("clicking upload");
+    await page.evaluate(() => {
+        const toClick = document.querySelector('[data-e2e="upload-icon"]')?.children[0];
+        if (toClick) {
+            toClick.click();
+        }
+    });
+    await delay(5000);
+    await page.pdf({ path: './screenshots/uploadPage.pdf' });
+};
+const setCookies = async (page, cookiesFile) => {
+    console.log("setting cookies");
+    const cookiesString = await fs.readFile(cookiesFile);
+    const cookies = JSON.parse(cookiesString);
+    await page.setCookie(...cookies);
 };
 /**
- * Given an account & password, upload a file to TikTok
- * @param account tiktok account
- * @param password tiktok password
- * @param file path to file to upload
+ * Given cookie file and video file, upload a video to TikTok
+ * @param cookiesFile path to cookies file which autheticates a TikTok user
+ * @param videoFile path to file to upload
  */
-async function uploadToTikTok(account, password, file) {
-    const browser = await puppeteer.launch();
+async function uploadToTikTok(cookiesFile, videoFile) {
+    const browser = await puppeteer_extra_1.default.launch({
+        defaultViewport: null,
+        ignoreHTTPSErrors: true,
+        slowMo: 100,
+    });
     const page = await browser.newPage();
-    await goToTikTok(page);
-    await login(page, account, password);
+    await setCookies(page, cookiesFile);
+    await visitTikTok(page);
+    await clickUpload(page);
     await browser.close();
 }
-const account = "username";
-const password = "password";
-const file = "upload_queue/video.mp4";
+/**
+ * Run with "npm start username password filepath"
+ */
 const run = async () => {
-    await uploadToTikTok(account, password, file);
+    const cookiesFile = 'src/cookies.json';
+    const videoFile = './upload_queue/video.mp4';
+    await uploadToTikTok(cookiesFile, videoFile);
 };
 run();
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,70 @@
 
-import * as puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
 import { Browser, Page } from 'puppeteer';
+const fs = require('fs').promises;
 
-const goToTikTok = async (page : Page) =>{
-    await page.goto('https://tiktok.com');
-    await page.pdf({path: 'tiktok.pdf'});
+const TIKTOKURL = "https://www.tiktok.com"
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
-const login = async(page : Page, account : String, password : String) => {
-
+const visitTikTok = async(page : Page) =>{
+    console.log("navigating to tiktok")
+    await page.goto(TIKTOKURL,{
+            waitUntil: 'networkidle0' 
+        }
+    );
+    
+    await page.pdf({path : './screenshots/tiktok.pdf'})
 }
 
+const clickUpload = async (page : Page) => {
+    console.log("clicking upload")
+    await page.evaluate(() => {
+        const toClick : HTMLAnchorElement = document.querySelector('[data-e2e="upload-icon"]')?.children[0] as HTMLAnchorElement;
+        if (toClick){
+            toClick.click();
+        }
+    })
+    await delay(5000);
+    await page.pdf({path : './screenshots/uploadPage.pdf'})
+}
+const setCookies= async (page : Page, cookiesFile : string) =>{
+    console.log("setting cookies")
+    const cookiesString = await fs.readFile(cookiesFile);
+    const cookies = JSON.parse(cookiesString);
+    await page.setCookie(...cookies);
 
+}
 
 /**
- * Given an account & password, upload a file to TikTok
- * @param account tiktok account
- * @param password tiktok password
- * @param file path to file to upload
+ * Given cookie file and video file, upload a video to TikTok
+ * @param cookiesFile path to cookies file which autheticates a TikTok user
+ * @param videoFile path to file to upload
  */
-async function uploadToTikTok(account : String, password : String, file : String){
-    const browser : Browser = await puppeteer.launch();
+async function uploadToTikTok(cookiesFile : string, videoFile : string){
+    const browser : Browser = await puppeteer.launch(
+        {
+            defaultViewport: null,
+            ignoreHTTPSErrors: true,
+            slowMo: 100,
+        }
+    );
     const page : Page  = await browser.newPage();
-    await goToTikTok(page);
-    await login(page, account, password);
+    await setCookies(page, cookiesFile);
+    await visitTikTok(page);
+    await clickUpload(page);
     await browser.close();
 }
 
-
-const account : String  = "username";
-const password : String  = "password";
-const file : String  = "upload_queue/video.mp4";
+/**
+ * Run with "npm start username password filepath"
+ */
 const run = async() =>{
-    await uploadToTikTok(account, password, file);
+    const cookiesFile = 'src/cookies.json';
+    const videoFile = './upload_queue/video.mp4';
+    await uploadToTikTok(cookiesFile, videoFile);
 }
 
 run();
